c9dec878c074604d4d700c9cfe7fa385dadac991
Joel  Godoy <j.godoy.moraes@gmail.com>
2023-01-13 18:34:11 -0300

    Chess layer and printing the board

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,13 +1,17 @@
 package Application;
 
-import Boardgame.Position;
+import Chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Board board = new board(8,8);
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
+		
+		
 	}
 
 }
--- src/Boardgame/Piece.java
+++ src/Boardgame/Piece.java
@@ -4,8 +4,14 @@ public class Piece {
 	//We don't want this matrix position to be seen by the chess layer
 	protected Position position;
 	private Board board;
+	
 	public Piece(Board board) {
 		this.board = board;
+		this.position = null;
+	}
+	//We dont want to expose the board to the chess layer, just to Pieces subclasses
+	protected Board getBoard() {
+		return board;
 	}
 	
 	
